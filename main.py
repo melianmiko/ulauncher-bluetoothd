7a990e96aaf8fed6aac72bad1c64facebd5a69a3
MelianMiko <melianmiko@gmail.com>
2021-09-11 19:31:33 +0700

    Small refactor

--- main.py
+++ main.py
@@ -11,63 +11,72 @@ from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 
 logger = logging.getLogger(__name__)
+notify_props = "--icon=" + os.path.dirname(os.path.realpath(__file__)) + "/images/icon.png"
+
+
+def send_notification(text):
+    logger.debug(text)
+    subprocess.run(["notify-send", notify_props, text])
 
 
 class BluetoothExtension(Extension):
     def __init__(self):
         super(BluetoothExtension, self).__init__()
+
+        # Subscribe plugin listeners to launcher
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
         self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
 
-class ItemEnterEventListener(EventListener):
-    def on_event(self, event, extension):
-        root = os.path.dirname(os.path.realpath(__file__))
-        iconprop = "--icon=" + root + "/images/icon.png"
-
-        data = event.get_data()
-        action_name = "connect"
-        if data[2]:
-            action_name = "disconnect"
-
-        # Get rfkill state
-        res = subprocess.run(["rfkill"], stdout=subprocess.PIPE)
-        res = str(res.stdout, "utf-8").split("\n")
-        for a in res:
-            rfdata = ' '.join(a.split())
-            rfdata = rfdata.split(" ")
-            if len(rfdata) > 1:
-                if rfdata[1] == "bluetooth":
-                    if rfdata[3] == "blocked":
-                        subprocess.run(["notify-send", iconprop, 'Enabling BT controller...'])
-                        subprocess.run(["rfkill", "unblock", rfdata[0]])
-                        time.sleep(2)
-
-        subprocess.run(['notify-send', iconprop, 'Trying to ' + action_name + '...', data[1]], stdout=subprocess.PIPE)
-        res = subprocess.run(['bluetoothctl', action_name, data[0]], stdout=subprocess.PIPE)
-        subprocess.run(["notify-send", iconprop, "BT " + action_name + " result is", str(res.stdout, "utf-8")],
-                       stdout=subprocess.PIPE)
-
-
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
-        items = []
+        # Check current action key
+        disconnect_key = extension.preferences.get("bt_kw2")
+        action_name = "disconnect" if event.get_keyword() == disconnect_key else "connect"
+
+        # Get list of paired devices from bluetoothctl output
         result = subprocess.run(['bluetoothctl', 'paired-devices'], stdout=subprocess.PIPE)
         lines = str(result.stdout, "utf-8").split("\n")
-        disconnect_key = extension.preferences.get("bt_kw2")
 
+        # Parse devices list and generate items list
+        items = []
         for a in lines:
             columns = a.split(" ")
             if len(columns) > 2:
                 name = ' '.join(columns[2:])
-                items.append(ExtensionResultItem(icon='images/icon.png',
-                                                 name=name,
-                                                 description=columns[1],
-                                                 on_enter=ExtensionCustomAction(
-                                                     (columns[1], name, event.get_keyword() == disconnect_key),
-                                                     keep_app_open=False)))
+                on_click_event = ExtensionCustomAction((columns[1], name, action_name), keep_app_open=False)
+                item_row = ExtensionResultItem(icon='images/icon.png',
+                                               name=name,
+                                               description=columns[1],
+                                               on_enter=on_click_event)
+
+                items.append(item_row)
 
         return RenderResultListAction(items)
+
+
+class ItemEnterEventListener(EventListener):
+    def on_event(self, event, extension):
+        # Check, that bluetooth controller is enabled
+        rfkill_data = subprocess.run(["rfkill"], stdout=subprocess.PIPE)
+        rfkill_data = str(rfkill_data.stdout, "utf-8").split("\n")
+        for a in rfkill_data:
+            rfkill_line = ' '.join(a.split())
+            rfkill_line = rfkill_line.split(" ")
+            if len(rfkill_line) > 1:
+                if rfkill_line[1] == "bluetooth" and rfkill_line[3] == "blocked":
+                    send_notification('Enabling BT controller...')
+
+                    # Re-enable bluetooth
+                    subprocess.run(["rfkill", "unblock", rfkill_line[0]])
+                    time.sleep(2)
+
+        # Get row data
+        address, name, action = event.get_data()
+
+        # Connect and send notification
+        result = subprocess.run(['bluetoothctl', action, address], stdout=subprocess.PIPE)
+        send_notification("BT " + action + " result is " + str(result.stdout, "utf-8"))
 
 
 if __name__ == '__main__':
