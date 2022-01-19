fe3b05f16197f4804dd35fd5357dfa38f8051d02
MelianMiko <melianmiko@gmail.com>
2022-01-19 17:40:20 +0700

    Fix: "Icon" prop not found

--- bt_tools.py
+++ bt_tools.py
@@ -38,16 +38,39 @@ def get_devices():
         if "org.bluez.Device1" in all_objects[path]:
             device1 = dbus.Interface(system.get_object("org.bluez", path),
                                      "org.freedesktop.DBus.Properties")
+            props = dbus_to_python(device1.GetAll("org.bluez.Device1"))
 
             devices.append({
-                "name": str(device1.Get("org.bluez.Device1", "Name")),
-                "uuid": str(device1.Get("org.bluez.Device1", "Address")),
-                "icon": str(device1.Get("org.bluez.Device1", "Icon")),
-                "active": bool(device1.Get("org.bluez.Device1", "Connected")),
+                "name": props.get("Name", "Unnamed device"),
+                "uuid": props.get("Address", "Unknown address"),
+                "icon": props.get("Icon", "default"),
+                "active": props.get("Connected", False),
                 "dbus_path": str(path)
             })
 
     return devices
+
+
+# From https://stackoverflow.com/questions/11486443/dbus-python-how-to-get-response-with-native-types
+def dbus_to_python(data):
+    """convert dbus data types to python native data types"""
+    if isinstance(data, dbus.String):
+        data = str(data)
+    elif isinstance(data, dbus.Boolean):
+        data = bool(data)
+    elif isinstance(data, dbus.Int64):
+        data = int(data)
+    elif isinstance(data, dbus.Double):
+        data = float(data)
+    elif isinstance(data, dbus.Array):
+        data = [dbus_to_python(value) for value in data]
+    elif isinstance(data, dbus.Dictionary):
+        new_data = dict()
+        for key in data.keys():
+            new_key = dbus_to_python(key)
+            new_data[new_key] = dbus_to_python(data[key])
+        data = new_data
+    return data
 
 
 if __name__ == "__main__":
